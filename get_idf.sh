b0ef92aacccfde430f81d845a6f6f6b8ec65e762
francis2tm <franciscomelot@hotmail.com>
2022-07-21 00:47:37 +0100

    add install argument to get_idf.sh

--- get_idf.sh
+++ get_idf.sh
@@ -6,8 +6,35 @@ SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 export IDF_PATH="$SCRIPT_PATH/deps/esp-idf/"
 export IDF_TOOLS_PATH="$SCRIPT_PATH/esp-idf-tools/"
 
-#If an argument is not supplited (don't skip the export part)
+
 if [ $# -eq 0 ]
+    #If an argument is not supplited (don't skip the export part)
     then
     . $IDF_PATH/export.sh
+
+    #Check for arguments
+    else
+    POSITIONAL_ARGS=()
+    while [[ $# -gt 0 ]]; do
+        case $1 in
+            --install)
+            INSTALL=1
+            shift # past argument
+            ;;
+            -*|--*)
+            echo "Unknown option $1"
+            exit 1
+            ;;
+            *)
+            POSITIONAL_ARGS+=("$1") # save positional arg
+            shift # past argument
+            ;;
+        esac
+    done
+
+    set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
+
+    if [ "$INSTALL" -eq "1" ]; then
+        ${SCRIPT_PATH}/deps/esp-idf/install.sh
+    fi
 fi
