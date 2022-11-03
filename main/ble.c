4b2b9fc7a0f14d4ee735388edd3812705b2bc7ba
francis2tm <franciscomelot@hotmail.com>
2022-11-03 21:12:27 +0000

    removed sendData() in sendBLE()

--- main/ble.c
+++ main/ble.c
@@ -492,8 +492,6 @@ static void gatts_profile_a_event_handler(esp_gatts_cb_event_t event, esp_gatt_i
     case ESP_GATTS_CONF_EVT:
         DEBUG_PRINT_I(GATTS_TAG, "ESP_GATTS_CONF_EVT, status %d", param->conf.status);
 
-        finalizeSend();
-
         //Try to send next buff
         if(!(param->congest.congested)){         //bt write is free   
             sendData();                             
@@ -553,8 +551,10 @@ esp_err_t IRAM_ATTR sendBle(uint32_t fd, int len, uint8_t *buff){
         DEBUG_PRINT_E("sendBle", "ERROR: SEND FAILED\n");
     }
 
+    finalizeSend();
+
     // Try to send next buff
-    sendData();
+    //sendData();
     return ESP_OK;
 }
 
