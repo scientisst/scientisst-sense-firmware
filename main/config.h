96cf03c84b7009f2f0087a6ccb832ec8c2d92015
kikofmas <kikofmas@gmail.com>
2023-06-07 16:59:15 +0100

    Updated config.h to include settings for SD card usage, isolated SD card related code inside compilation macros

--- main/config.h
+++ main/config.h
@@ -4,13 +4,13 @@
 #define NO_EXT_ADC 0  // External ADC disabled
 #define ADC_ADS 1     // ADS
 #define ADC_MCP 2
-#define _ADC_EXT_ NO_EXT_ADC
+#define _ADC_EXT_ ADC_MCP
 
 #define HW_VERSION_LEGACY 0
 #define HW_VERSION_NANO 1
 #define HW_VERSION HW_VERSION_LEGACY
 
-#define _SD_CARD_ENABLED_ 1  // 0: sd card disabled, 1: sd card enabled
+#define _SD_CARD_ENABLED_ 0  // 0: sd card disabled, 1: sd card enabled
 #define FORMAT_SDCARD_IF_MOUNT_FAILED 0
 // 0: do not format sd card if mount failed, 1: format sd card if mount
 // failed
